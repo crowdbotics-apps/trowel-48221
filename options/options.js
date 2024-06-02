7f8bdccf172dfdae05e15ee7b8959701aa416d84
Crowdbotics <team@crowdbotics.com>
2024-06-02 16:09:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trowel-48221",
+  url: "https://trowel-48221.botics.co",
+  api: "https://trowel-48221.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
